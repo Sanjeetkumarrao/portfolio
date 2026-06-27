d890144384fa27c0d9b2446a310334a0ae70fa34
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2026-06-27 07:11:01 +0530

    base url updated

--- server/server.js
+++ server/server.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({ 
+  origin: 'http://localhost:5173', 
+  // origin : "", 
+  credentials: true }));
+
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true }));
 
